eeabc88af9027446b3f58a8bc3a4b9a42840b07f
Karl Heitmann <khf1988@gmail.com>
2023-05-08 22:35:36 -0400

    compiles basic magnus

--- bin/rev.rb
+++ bin/rev.rb
@@ -2,5 +2,9 @@
 
 require 'rust_reverse'
 # puts RustReverse.reverse(ARGV[0].to_i)
-puts RustReverse.reverse(ARGV[0])
+# puts RustReverse.distance(ARGV[0])
+# puts RustReverse.send(:distance(ARGV[0]))
+#
+puts RustReverse.send(:distance, [0, 0], [3,4])
+# puts RustReverse.distance([1, 2], [1,3])
 
