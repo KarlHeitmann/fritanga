a5499e7889469da05b19e0cf6cd7591e23fc6d1d
Karl Heitmann <khf1988@gmail.com>
2023-05-24 19:52:20 -0400

    Refactor rust serializer

--- ext/fritanga/src/lib.rs
+++ ext/fritanga/src/lib.rs
@@ -1,38 +1,34 @@
+// READ THIS: https://bundler.io/blog/2023/01/31/rust-gem-skeleton.html
+
 use magnus::{define_global_function, function, Value, RArray, Symbol};
 use serde_json::{json, Value as JsonValue};
 
+fn value_serializer_to_json(serializer: Value) -> JsonValue {
+    let mut js_nested:JsonValue = json!({});
+    let functions: RArray = serializer.funcall("descriptor", ()).unwrap();
+    for function in functions.each() {
+        let function = function.unwrap();
+        let method_name_symbol = function.try_convert::<Symbol>().unwrap();
+        let data: String = serializer.funcall(method_name_symbol, ()).unwrap();
+        let method_name: String =  method_name_symbol.name().unwrap().to_string();
+        js_nested[method_name] = serde_json::Value::String(data);
+    }
+    js_nested
+}
+
 pub fn serialize_array(builders: RArray) -> String {
-    let mut js = json!([]);
     let mut tmp: Vec<JsonValue> = vec![];
     for builder in builders.each() {
         let builder = builder.unwrap();
-        let mut js_nested:JsonValue = json!({});
         let serializer:Value = builder.funcall("call", ()).unwrap();
-        let d:String = serializer.funcall("inspect", ()).unwrap();
-        let functions: RArray = serializer.funcall("descriptor", ()).unwrap();
-        for function in functions.each() {
-            let function = function.unwrap();
-            let method_name_symbol = function.try_convert::<Symbol>().unwrap();
-            let data: String = serializer.funcall(method_name_symbol, ()).unwrap();
-            let method_name =  method_name_symbol.name().unwrap().to_ascii_lowercase();
-            js_nested[method_name] = serde_json::Value::String(data);
-        }
+        let js_nested = value_serializer_to_json(serializer);
         tmp.push(js_nested);
     }
     json!(tmp).to_string()
 }
 
-pub fn serialize(a: Value) -> String {
-    let output: RArray = a.funcall("descriptor", ()).unwrap();
-    let mut js = json!({});
-    for i in output.each() {
-        let i = i.unwrap();
-        let method_name_symbol = i.try_convert::<Symbol>().unwrap();
-        let data: String = a.funcall(method_name_symbol, ()).unwrap();
-        let method_name =  method_name_symbol.name().unwrap().to_ascii_lowercase();
-        js[method_name] = serde_json::Value::String(data);
-    }
-    js.to_string()
+pub fn serialize(serializer: Value) -> String {
+    value_serializer_to_json(serializer).to_string()
 }
 
 #[magnus::init]
