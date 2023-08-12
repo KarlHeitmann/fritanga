0dab62352d898e95aa0b54af9567d25055eb80a9
Karl Heitmann <khf1988@gmail.com>
2023-08-12 15:07:34 -0400

    Downgraded magnus to version 0.4 in order to compile

--- ext/fritanga/extconf_bare.rb
+++ ext/fritanga/extconf_bare.rb
@@ -1,6 +1,3 @@
-# Use local rb_sys gem (only needed for developing in this repo)
-$LOAD_PATH.unshift(File.expand_path("../../../../gem/lib", __dir__))
-
 # We need to require mkmf *first* this since `rake-compiler` injects code here for cross compilation
 require "mkmf"
 require "rb_sys/mkmf"
